91fc24c0c456f79cdea7b89d183c942147c52069
kesel <a.kiselyoff@gmail.com>
2022-05-30 19:01:18 +0300

    add Statistics component jsx
    + add randomColor fn

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
+import getRandomHexColor from '././helpers/randomColor';
+
+function Statistic({ title, stats }) {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+      <ul className={s.statList}>
+        {stats.map(stat => (
+          <li
+            className={s.item}
+            key={stat.id}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <span className={s.label}>{stat.label}</span>
+            <span className={s.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Statistic;
+
+Statistic.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      label: PropTypes.string,
+      percentage: PropTypes.number,
+    }),
+  ),
+};
