204e0c02319b798f41489625800fbea57565e840
kesel <a.kiselyoff@gmail.com>
2022-05-30 19:52:47 +0300

    add Friends styles

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -23,10 +23,10 @@ export default function FriendList({ friends }) {
 FriendList.propTypes = {
   friends: PropTypes.arrayOf(
     PropTypes.shape({
-      avatar: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      isOnline: PropTypes.bool.isRequired,
-      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      isOnline: PropTypes.bool,
+      id: PropTypes.number,
     }),
   ),
 };
