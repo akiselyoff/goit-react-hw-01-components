69b1f388fd7b3dfaec7db98b0ac1bedb6dc6d78e
kesel <a.kiselyoff@gmail.com>
2022-05-30 19:56:47 +0300

    create Transactions component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
 import FriendList from './components/FriendList/FriendList';
+import TransactionHistory from './components/TransactionHistory';
 import user from './data/user.json';
 import data from './data/data.json';
 import friends from './data/friens.json';
+import transactions from './data/transactions.json';
 export default function App() {
   return (
     <>
