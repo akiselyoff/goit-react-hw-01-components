52731f3222a62a722d1a6ad005b142bb0e9ea4a3
kesel <a.kiselyoff@gmail.com>
2022-05-30 19:36:52 +0300

    add Friends component markup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
+import FriendList from './components/FriendList/FriendList';
 import user from './data/user.json';
 import data from './data/data.json';
+import friends from './data/friens.json';
 export default function App() {
   return (
     <>
@@ -14,6 +16,7 @@ export default function App() {
         //   {...user} alternative use props
       />
       <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
     </>
   );
 }
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import s from './FriendList.module.css';
+
+export default function FriendList({ friends }) {
+  return (
+    <ul className={s.list}>
+      {friends.map(friend => (
+        <li className={s.item} key={friend.id}>
+          <span className={friend.isOnline ? s.online : s.offline}></span>
+          <img
+            className={s.avatar}
+            src={friend.avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={s.name}>{friend.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
+    }),
+  ),
+};
